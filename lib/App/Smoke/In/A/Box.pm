7713697c02946670a8f244036686eb913fadfad8
Chris Williams <chris@bingosnet.co.uk>
2009-05-14 23:44:51 +0100

    Started with CPAN distribution layout

---
+++ lib/App/Smoke/In/A/Box.pm
@@ -0,0 +1,27 @@
+package App::Smoke::In::A::Box;
+
+use strict;
+use warnings;
+use vars qw($VERSION);
+
+$VERSION = '0.02';
+
+'Smokebox';
+
+__END__
+
+=head1 NAME
+
+App::Smoke::In::A::Box - CPAN Tester smoke environment boxed
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 MAINTAINER
+
+=head1 LICENSE
+
+=head1 SEE ALSO
+
+=cut
